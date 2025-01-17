8a98bd9c677e05b4c2033cc9225502a3650b3015
Randson Vitorino <81572283+randsonvitorino@users.noreply.github.com>
2025-01-17 15:31:09 -0300

    Update lontraads-smart-tracker.js

--- lontraads-smart-tracker.js
+++ lontraads-smart-tracker.js
@@ -1,4 +1,6 @@
 (function() {
+    var originUrl = document.currentScript.getAttribute('data-origin-url');
+
     function getAllUrlParams() {
         var queryString = window.location.search.slice(1);
         var obj = {};
@@ -15,56 +17,24 @@
         return obj;
     }
 
-    function setCookie(name, value, days) {
-        var expires = "";
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days*24*60*60*1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-    }
-
-    function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-
     var urlParams = getAllUrlParams();
-    var storedData = {};
 
-    for (var param in urlParams) {
-        storedData[param] = urlParams[param];
-        setCookie(param, urlParams[param], 30);
-    }
-
+    // Modificar links
     var links = document.getElementsByTagName('a');
     for (var i = 0; i < links.length; i++) {
         var href = links[i].href;
         var separator = href.indexOf('?') !== -1 ? '&' : '?';
-        for (var key in storedData) {
-            href += separator + key + '=' + encodeURIComponent(storedData[key]);
+        for (var key in urlParams) {
+            href += separator + key + '=' + encodeURIComponent(urlParams[key]);
             separator = '&';
         }
         links[i].href = href;
     }
 
-    var currentDomain = window.location.hostname;
-    var pluginDomain = 'chatwavedigital.com.br';
-    if (currentDomain !== pluginDomain) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://chatwavedigital.com.br/wp-admin/admin-ajax.php', true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        var data = 'action=lontraads_record_visit&domain=' + encodeURIComponent(currentDomain) + '&user_agent=' + encodeURIComponent(navigator.userAgent);
-        for (var key in storedData) {
-            data += '&' + key + '=' + encodeURIComponent(storedData[key]);
-        }
-        xhr.send(data);
-    }
+    // Enviar dados de IP
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', originUrl + '/wp-admin/admin-ajax.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    var data = 'action=lontraads_record_visit&domain=' + encodeURIComponent(window.location.hostname) + '&user_agent=' + encodeURIComponent(navigator.userAgent);
+    xhr.send(data);
 })();
