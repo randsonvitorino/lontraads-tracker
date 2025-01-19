60abb480723d07d1425eb3a221016d99534fad58
Randson Vitorino <81572283+randsonvitorino@users.noreply.github.com>
2025-01-19 16:43:19 -0300

    Update lontraads-smart-tracker.js

--- lontraads-smart-tracker.js
+++ lontraads-smart-tracker.js
@@ -1,40 +1,85 @@
 (function() {
-    var originUrl = document.currentScript.getAttribute('data-origin-url');
+    function sendVisitorData() {
+        var scriptElement = document.querySelector('script[data-origin-url]');
+        var originUrl = scriptElement ? scriptElement.getAttribute('data-origin-url') : null;
 
-    function getAllUrlParams() {
-        var queryString = window.location.search.slice(1);
-        var obj = {};
-        if (queryString) {
-            queryString = queryString.split('#')[0];
-            var arr = queryString.split('&');
-            for (var i = 0; i < arr.length; i++) {
-                var a = arr[i].split('=');
-                var paramName = a[0];
-                var paramValue = typeof (a[1]) === 'undefined' ? true : decodeURIComponent(a[1]);
-                obj[paramName] = paramValue;
+        if (!originUrl) {
+            console.error('Origin URL não encontrada. O script não pode prosseguir.');
+            return;
+        }
+
+        var currentDomain = window.location.hostname;
+        var pluginDomain = new URL(originUrl).hostname;
+
+        if (currentDomain !== pluginDomain) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', originUrl + '/wp-admin/admin-ajax.php', true);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        console.log('Visita registrada com sucesso');
+                    } else {
+                        console.error('Erro ao registrar visita');
+                    }
+                }
+            };
+
+            var referrer = document.referrer || '';
+            var campaignId = getCampaignId();
+
+            var data = 'action=lontraads_record_visit' +
+                       '&domain=' + encodeURIComponent(currentDomain) +
+                       '&user_agent=' + encodeURIComponent(navigator.userAgent) +
+                       '&referrer=' + encodeURIComponent(referrer) +
+                       '&campaign_id=' + encodeURIComponent(campaignId);
+            xhr.send(data);
+        } else {
+            console.log('Visita não registrada: domínio do plugin');
+        }
+    }
+
+    function getCampaignId() {
+        var urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get('campaign_id') || '';
+    }
+
+    function enhancePageLinks() {
+        var scriptElement = document.querySelector('script[data-origin-url]');
+        var originUrl = scriptElement ? scriptElement.getAttribute('data-origin-url') : null;
+
+        if (!originUrl) {
+            console.error('Origin URL não encontrada. O script não pode prosseguir.');
+            return;
+        }
+
+        var queryParams = new URLSearchParams(window.location.search);
+        var campaignId = getCampaignId();
+
+        var pageLinks = document.getElementsByTagName('a');
+        for (var i = 0; i < pageLinks.length; i++) {
+            var link = pageLinks[i];
+            var linkUrl = new URL(link.href);
+            var hashPart = linkUrl.hash;
+            linkUrl.search = new URLSearchParams([...queryParams, ...new URLSearchParams(linkUrl.search)]);
+            
+            if (campaignId) {
+                linkUrl.searchParams.set('campaign_id', campaignId);
             }
+
+            link.href = linkUrl.toString().split('#')[0] + hashPart;
         }
-        return obj;
     }
 
-    var urlParams = getAllUrlParams();
-
-    // Modificar links
-    var links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        var href = links[i].href;
-        var separator = href.indexOf('?') !== -1 ? '&' : '?';
-        for (var key in urlParams) {
-            href += separator + key + '=' + encodeURIComponent(urlParams[key]);
-            separator = '&';
-        }
-        links[i].href = href;
+    if (!sessionStorage.getItem('lontraads_visit_recorded')) {
+        sendVisitorData();
+        sessionStorage.setItem('lontraads_visit_recorded', 'true');
     }
 
-    // Enviar dados de IP
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', originUrl + '/wp-admin/admin-ajax.php', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    var data = 'action=lontraads_record_visit&domain=' + encodeURIComponent(window.location.hostname) + '&user_agent=' + encodeURIComponent(navigator.userAgent);
-    xhr.send(data);
+    window.addEventListener('beforeunload', function() {
+        sessionStorage.removeItem('lontraads_visit_recorded');
+    });
+
+    enhancePageLinks();
 })();
