49adb4d4660fb4c4128fc21747ff44c8af95bfca
Randson Vitorino <81572283+randsonvitorino@users.noreply.github.com>
2025-01-21 15:22:38 -0300

    Update lontraads-smart-tracker.js

--- lontraads-smart-tracker.js
+++ lontraads-smart-tracker.js
@@ -4,25 +4,13 @@
     var originUrl = document.currentScript.getAttribute('data-origin-url');
 
     function getAllUrlParams() {
-        var queryString = window.location.search.slice(1);
-        var obj = {};
-        if (queryString) {
-            queryString = queryString.split('#')[0];
-            var arr = queryString.split('&');
-            for (var i = 0; i < arr.length; i++) {
-                var a = arr[i].split('=');
-                var paramName = a[0];
-                var paramValue = typeof (a[1]) === 'undefined' ? true : decodeURIComponent(a[1]);
-                obj[paramName] = paramValue;
-            }
-        }
-        return obj;
+        return new URLSearchParams(window.location.search);
     }
 
     function enhancePageLinks() {
         console.log("Iniciando enhancePageLinks");
         var urlParams = getAllUrlParams();
-        console.log("Parâmetros da URL atual:", JSON.stringify(urlParams));
+        console.log("Parâmetros da URL atual:", urlParams.toString());
 
         var links = document.getElementsByTagName('a');
         console.log("Total de links encontrados:", links.length);
@@ -31,22 +19,15 @@
             var link = links[i];
             var originalHref = link.href;
             
-            // Separar a URL base do hash
-            var [baseUrl, hash] = originalHref.split('#');
-            var url = new URL(baseUrl, window.location.href);
+            var url = new URL(originalHref, window.location.href);
             
-            // Adicionar ou atualizar parâmetros
-            for (var key in urlParams) {
-                if (key === 'campaign_id' || key === 'gclid' || key === 'fbclid' || key === 'msclkid' || key === 'wbraid') {
-                    url.searchParams.set(key, urlParams[key]);
-                }
-            }
+            urlParams.forEach(function(value, key) {
+                url.searchParams.set(key, value);
+            });
             
-            // Reconstruir a URL com o hash, se existir
-            var newHref = url.toString() + (hash ? '#' + hash : '');
-            link.href = newHref;
+            link.href = url.toString();
             
-            console.log(`Link ${i + 1} modificado:`, originalHref, "->", newHref);
+            console.log(`Link ${i + 1} modificado:`, originalHref, "->", link.href);
         }
 
         console.log("Modificação de links concluída");
@@ -79,29 +60,48 @@
                 }
             };
 
-            var campaignId = getAllUrlParams()['campaign_id'] || '';
-            console.log("Campaign ID:", campaignId);
-
+            var urlParams = getAllUrlParams();
             var data = 'action=lontraads_record_visit' +
-                       '&domain=' + encodeURIComponent(currentDomain) +
-                       '&campaign_id=' + encodeURIComponent(campaignId);
+                       '&domain=' + encodeURIComponent(currentDomain);
+
+            urlParams.forEach(function(value, key) {
+                data += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(value);
+            });
+
             xhr.send(data);
             console.log("Dados enviados:", data);
         }
     }
 
-    // Executa as funções principais
-    if (document.readyState === 'loading') {
-        console.log("DOM ainda carregando, adicionando evento listener");
-        document.addEventListener('DOMContentLoaded', function() {
-            enhancePageLinks();
-            sendVisitorData();
-        });
-    } else {
-        console.log("DOM já carregado, executando funções imediatamente");
+    function init() {
+        console.log("Iniciando funções principais");
         enhancePageLinks();
         sendVisitorData();
     }
+
+    if (document.readyState === 'loading') {
+        console.log("DOM ainda carregando, adicionando evento listener");
+        document.addEventListener('DOMContentLoaded', init);
+    } else {
+        console.log("DOM já carregado, executando funções imediatamente");
+        init();
+    }
+
+    // Adiciona um ouvinte para modificar links dinâmicos
+    document.addEventListener('click', function(e) {
+        if (e.target.tagName === 'A') {
+            var link = e.target;
+            var url = new URL(link.href, window.location.href);
+            var urlParams = getAllUrlParams();
+            
+            urlParams.forEach(function(value, key) {
+                url.searchParams.set(key, value);
+            });
+            
+            link.href = url.toString();
+            console.log("Link clicado modificado:", link.href);
+        }
+    }, true);
 
     console.log("Script concluído");
 })();
